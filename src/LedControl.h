3a59ff8001d6e37a171dbec608c390a7ceb0990a
Folkert van Heusden <folkert@vanheusden.com>
2026-02-20 18:33:53 +0100

    hardware spi test

--- src/LedControl.h
+++ src/LedControl.h
@@ -28,6 +28,7 @@
 #define LedControl_h
 
 #include <avr/pgmspace.h>
+#include <SPI.h>
 
 #if (ARDUINO >= 100)
 #include <Arduino.h>
@@ -67,10 +68,6 @@ class LedControl {
 
         /* We keep track of the led-status for all 8 devices in this array */
         byte status[64];
-        /* Data is shifted out of this pin*/
-        int SPI_MOSI;
-        /* The clock is signaled on this pin */
-        int SPI_CLK;
         /* This one is driven LOW for chip selectzion */
         int SPI_CS;
         /* The maximum number of devices we use */
